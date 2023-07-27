1bebc9913d42e33dc65a97a952e60c54281fa58e
rabuu <rabuu.dev@gmail.com>
2023-07-27 12:47:21 +0200

    manifest: work on targets

--- src/manifest.rs
+++ src/manifest.rs
@@ -21,8 +21,8 @@ pub struct Target {
     blocks: HashMap<String, Block>,
     comments: HashMap<String, Comment>,
     current_costume: u32,
-    costumes: Vec<Costume>,
-    sounds: Vec<Sound>,
+    costumes: Vec<Asset>,
+    sounds: Vec<Asset>,
     layer_order: u32,
     volume: u8,
 
@@ -33,12 +33,22 @@ pub struct Target {
     text_to_speech_language: Option<Language>,
 }
 
-type Variable = (String, i64, bool);
+type Variable = (String, i32, bool);
 type List = (String, Vec<String>);
 type Broadcast = String;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Block {
+#[serde(untagged)]
+pub enum Block {
+    Standard(StandardBlock),
+    // TODO: investigate different numeral modes
+    Simple(u8, String),
+    Advanced(u8, String, String),
+    AdvancedWithPos(u8, String, String, u32, u32),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StandardBlock {
     opcode: String,
     next: Option<String>,
     parent: Option<String>,
@@ -55,3 +65,102 @@ pub struct Block {
 // TODO
 type Input = ();
 type Field = ();
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Mutation {
+    tag_name: String,
+    children: [(); 0],
+
+    // "procedures_prototype" and "procedures_call"
+    proccode: Option<String>,
+    argumentids: Option<Vec<String>>,
+    warp: Option<bool>,
+
+    // "procedures_prototype"
+    argumentnames: Option<Vec<String>>,
+    argumentdefaults: Option<Vec<ArgDefault>>,
+
+    // "control_stop"
+    hasnext: Option<bool>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ArgDefault {
+    Str(String),
+    Bool(bool),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Comment {
+    block_id: String,
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
+    minimized: bool,
+    test: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Asset {
+    asset_id: String,
+    name: String,
+    md5ext: String,
+    data_format: String,
+
+    #[serde(flatten)]
+    asset_type: AssetType,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum AssetType {
+    Costume {
+        bitmap_resolution: Option<f32>,
+        rotation_center_x: u32,
+        rotation_center_y: u32,
+    },
+    Sound {
+        rate: f32,
+        sample_count: u32,
+    },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum VideoState {
+    #[serde(rename = "on")]
+    On,
+    #[serde(rename = "off")]
+    Off,
+    #[serde(rename = "on-flipped")]
+    OnFlipped,
+}
+
+// TODO: incomplete
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Language {
+    Danish,
+    Dutch,
+    English,
+    French,
+    German,
+    Icelandic,
+    Italian,
+    Japanese,
+    Polish,
+    PortugueseBr,
+    PortugueseEu,
+    Russian,
+    SpanishEu,
+    SpanishLat,
+    ChineseMan,
+    Korean,
+    Norwegian,
+    Romanian,
+    Swedish,
+    Turkish,
+    Welsh,
+    Hindi,
+    Arabic,
+}
