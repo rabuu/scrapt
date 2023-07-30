4e513fa39696d390c6bd8ccdb84a058efd58cb03
rabuu <rabuu.dev@gmail.com>
2023-07-30 12:55:21 +0200

    unscratch manifest: some defaults

--- src/manifest/unscratch.rs
+++ src/manifest/unscratch.rs
@@ -5,6 +5,7 @@ use super::common::*;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Manifest {
     project: Project,
+    #[serde(default)]
     assets: Assets,
     meta: Metadata,
 }
@@ -12,11 +13,31 @@ pub struct Manifest {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Project {
     name: String,
+    #[serde(default)]
     extensions: Vec<Extension>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Assets {
+    #[serde(default = "default_assets_dir")]
     directory: String,
+    #[serde(default = "default_auto_renaming")]
     auto_renaming: bool,
+}
+
+impl Default for Assets {
+    fn default() -> Self {
+        Self {
+            directory: default_assets_dir(),
+            auto_renaming: default_auto_renaming(),
+        }
+    }
+}
+
+fn default_assets_dir() -> String {
+    String::from("assets")
+}
+
+fn default_auto_renaming() -> bool {
+    true
 }
