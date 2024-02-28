e60ceee67d8e72431a98a354871246716e10eeba
Rasmus Buurman <dev@rbuurman.de>
2024-02-28 18:55:39 +0100

    main: Prepare transpiler development

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-fn main() {
-    let m = std::fs::read_to_string("./manifest.json").unwrap();
-    let m = manifest_scratch::Manifest::parse(&m).unwrap();
+use std::env;
+use std::fs;
+use std::path;
 
-    println!("{:#?}", m);
+use manifest_scrapt::Manifest as ScraptManifest;
+
+fn main() {
+    let mut args = env::args().skip(1);
+
+    let project_path = args.next().unwrap();
+    let project_path = path::PathBuf::from(project_path);
+
+    let manifest_path = project_path.join("project.toml");
+    let manifest = ScraptManifest::parse(&fs::read_to_string(manifest_path).unwrap()).unwrap();
+
+    println!("{:#?}", manifest);
 }
