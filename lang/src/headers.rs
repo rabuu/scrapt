4f5525a9b394569dc10d654b982019ea00b47fd8
Rasmus Buurman <dev@rbuurman.de>
2024-09-03 03:36:14 +0100

    Add costumes/sound parsers

--- lang/src/headers.rs
+++ lang/src/headers.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 use std::path::PathBuf;
+use std::str::FromStr;
 
 use chumsky::prelude::*;
 use scratch_common_types::{AudioType, ImgType, Value};
@@ -125,6 +126,78 @@ pub fn broadcasts_header_parser<'tok, 'src: 'tok>() -> impl Parser<
                     }
                 }
                 broadcasts
+            })
+            .delimited_by(just(Token::CurlyOpen), just(Token::CurlyClose)),
+    )
+}
+
+pub fn costumes_header_parser<'tok, 'src: 'tok>(
+) -> impl Parser<'tok, ParserInput<'tok, 'src>, CostumesHeader, extra::Err<Rich<'tok, Token<'src>, Span>>>
+{
+    let img_type = select! { Token::Img(img) => img }.labelled("image type");
+
+    let path = select! {
+        Token::String(path) => path
+    }
+    .try_map(|p: &str, span| PathBuf::from_str(p).map_err(|e| Rich::custom(span, e)))
+    .labelled("image path");
+
+    let decl = ident()
+        .then(just(Token::Colon).ignore_then(img_type))
+        .then(just(Token::Equals).ignore_then(path).or_not())
+        .then_ignore(just(Token::Semicolon));
+
+    just(Token::Costumes).ignore_then(
+        decl.repeated()
+            .at_least(1)
+            .collect::<Vec<_>>()
+            .validate(|decls, _, emitter| {
+                let mut costumes = HashMap::new();
+                for (((id, span), file_type), path) in decls {
+                    if costumes.insert(id.clone(), (file_type, path)).is_some() {
+                        emitter.emit(Rich::custom(
+                            span,
+                            format!("Broadcast '{id}' declared twice"),
+                        ));
+                    }
+                }
+                costumes
+            })
+            .delimited_by(just(Token::CurlyOpen), just(Token::CurlyClose)),
+    )
+}
+
+pub fn sounds_header_parser<'tok, 'src: 'tok>(
+) -> impl Parser<'tok, ParserInput<'tok, 'src>, SoundsHeader, extra::Err<Rich<'tok, Token<'src>, Span>>>
+{
+    let audio_type = select! { Token::Audio(audio) => audio }.labelled("audio type");
+
+    let path = select! {
+        Token::String(path) => path
+    }
+    .try_map(|p: &str, span| PathBuf::from_str(p).map_err(|e| Rich::custom(span, e)))
+    .labelled("audio path");
+
+    let decl = ident()
+        .then(just(Token::Colon).ignore_then(audio_type))
+        .then(just(Token::Equals).ignore_then(path).or_not())
+        .then_ignore(just(Token::Semicolon));
+
+    just(Token::Sounds).ignore_then(
+        decl.repeated()
+            .at_least(1)
+            .collect::<Vec<_>>()
+            .validate(|decls, _, emitter| {
+                let mut sounds = HashMap::new();
+                for (((id, span), file_type), path) in decls {
+                    if sounds.insert(id.clone(), (file_type, path)).is_some() {
+                        emitter.emit(Rich::custom(
+                            span,
+                            format!("Broadcast '{id}' declared twice"),
+                        ));
+                    }
+                }
+                sounds
             })
             .delimited_by(just(Token::CurlyOpen), just(Token::CurlyClose)),
     )
